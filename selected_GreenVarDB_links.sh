404dcdc7dd8bbc6caec875a9e2b930806ea8fe4b
sohamsg90 <sohamsengupta90@gmail.com>
2024-01-03 11:13:00 -0600

    update

--- selected_GreenVarDB_links.sh
+++ selected_GreenVarDB_links.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #https://github.com/edg1983/GREEN-VARAN/blob/master/resources/GREENDB_collection.txt
 
-wget -c GRCh38_TFBS regions https://zenodo.org/record/5705936/files/GRCh38_TFBS.merged.bed.gz
-wget -c GRCh38_TFBS regions https://zenodo.org/record/5705936/files/GRCh38_TFBS.merged.bed.gz.csi
-wget -c GRCh38_DNase regions https://zenodo.org/record/5705936/files/GRCh38_DNase.merged.bed.gz
-wget -c GRCh38_DNase regions https://zenodo.org/record/5705936/files/GRCh38_DNase.merged.bed.gz.csi
-wget -c GRCh38_GREENDB_bed GREENDB_bed https://zenodo.org/record/5636209/files/GRCh38_GREEN-DB.bed.gz
-wget -c GRCh38_GREENDB_bed GREENDB_bed https://zenodo.org/record/5636209/files/GRCh38_GREEN-DB.bed.gz.csi
+#GRCh38_TFBS regions
+wget -c https://zenodo.org/record/5705936/files/GRCh38_TFBS.merged.bed.gz
+wget -c https://zenodo.org/record/5705936/files/GRCh38_TFBS.merged.bed.gz.csi
+
+#GRCh38_DNase regions 
+wget -c https://zenodo.org/record/5705936/files/GRCh38_DNase.merged.bed.gz
+wget -c https://zenodo.org/record/5705936/files/GRCh38_DNase.merged.bed.gz.csi
+
+#GRCh38_GREENDB_bed GREENDB_bed 
+wget -c https://zenodo.org/record/5636209/files/GRCh38_GREEN-DB.bed.gz
+wget -c https://zenodo.org/record/5636209/files/GRCh38_GREEN-DB.bed.gz.csi
